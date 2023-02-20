83abab84e4522912c6a3897ec874eabf3dd52c56
magomed.khasanov <magomed.khasanov@lge.com>
2023-02-20 19:54:44 +0300

    add page loader, not found page

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,11 +1,12 @@
 import React, { Suspense } from 'react';
 import { useRoutes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routerConfig/routerConfig';
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
 
 const AppRouter = () => {
     const elements = useRoutes(Object.values(routeConfig));
     return (
-        <Suspense fallback={<div>Загрузка...</div>}>
+        <Suspense fallback={<PageLoader />}>
             <div className="page-wrapper">
                 { elements }
             </div>
