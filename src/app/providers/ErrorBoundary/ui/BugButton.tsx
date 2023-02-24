be12301bf2b20bb0448b3d5cdd35225e8a9e2877
magomed.khasanov <magomed.khasanov@lge.com>
2023-02-24 16:21:19 +0300

    add error boundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
+
+export const BugButton = () => {
+    const [error, setError] = useState(false);
+    const { t } = useTranslation();
+    const onThrow = () => {
+        setError(true);
+    };
+    useEffect(() => {
+        if (error) {
+            throw new Error('My error');
+        }
+    }, [error]);
+    return (
+        <Button
+            onClick={onThrow}
+        >
+            {t('Вызвать ошибку')}
+        </Button>
+    );
+};
